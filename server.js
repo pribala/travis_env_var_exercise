e91e7a8e067386e5c1408c03953ffa3e5ad2975b
Priya Bala <andu_pri@yahoo.com>
2020-12-17 00:08:01 -0500

    yml commit

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
+    const favoriteFood = process.env.NAME;
+console.log(`My favorite food is ${favoriteFood}`);
     console.log("Microservices rock!");
     await sleep(5000);
   }
